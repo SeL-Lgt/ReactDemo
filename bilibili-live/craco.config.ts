ded13293b2d6a38fd0f69c6f89b9169e4e236879
SeL-Lgt <122790345@qq.com>
2022-10-30 14:16:19 +0800

    feat: 自适应配置

--- bilibili-live/craco.config.ts
+++ bilibili-live/craco.config.ts
@@ -5,6 +5,7 @@ const {whenDev, whenProd} = require("@craco/craco")
 const {BundleAnalyzerPlugin} = require("webpack-bundle-analyzer")
 const CompressionWebpackPlugin = require("compression-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const pxToViewport = require('postcss-px-to-viewport-8-plugin')
 
 const isDev = process.env.NODE_ENV === "development"
 
@@ -62,6 +63,30 @@ module.exports = {
         }),
         ...isDev ? whenDevPlugin : whenProdPlugin
       ]
+    }
+  },
+  style: {
+    postcss: {
+      mode: "extends",
+      loaderOptions: {
+        postcssOptions: {
+          ident: 'postcss',
+          plugins: [pxToViewport({
+            unitToConvert: 'px', // 要转化的单位
+            viewportWidth: 375, // UI设计稿的宽度
+            unitPrecision: 6, // 转换后的精度，即小数点位数
+            propList: ['*'], // 指定转换的css属性的单位，*代表全部css属性的单位都进行转换
+            viewportUnit: 'vw', // 指定需要转换成的视窗单位，默认vw
+            fontViewportUnit: 'vw', // 指定字体需要转换成的视窗单位，默认vw
+            selectorBlackList: ['wrap'], // 指定不转换为视窗单位的类名，
+            minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
+            mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
+            replace: true, // 是否转换后直接更换属性值
+            exclude: [/node_modules/], // 设置忽略文件，用正则做目录名匹配
+            landscape: false, // 是否处理横屏情况
+          })],
+        },
+      }
     }
   },
 }
