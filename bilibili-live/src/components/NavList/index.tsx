e3ba116e748610d18216e51eca5dc8481068013b
SeL-Lgt <122790345@qq.com>
2022-11-07 00:09:21 +0800

    feat: 添加路由导航优化

--- bilibili-live/src/components/NavList/index.tsx
+++ bilibili-live/src/components/NavList/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { Tabs } from 'antd-mobile';
+import { Dropdown, Tabs } from 'antd-mobile';
 import { BaseState } from '@/mock/interface';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import './index.less';
-import { DownOutline } from 'antd-mobile-icons';
+import { DownOutline, UpOutline } from 'antd-mobile-icons';
+import { DropdownRef } from 'antd-mobile/es/components/dropdown';
 
 type PropType = {
   baseReducer: BaseState;
@@ -17,15 +18,25 @@ export function NavList(props: PropType) {
   const { pathname } = location;
   const { navList } = baseReducer;
   const [children, setChildren] = useState<NavItem[]>([]);
-  const [actionKey, setActionKey] = useState<string>('');
+  const [childrenActiveKey, setChildrenActiveKey] = useState<string>('');
+  const [parentActiveKey, setParentActiveKey] = useState<string>('');
+  const ref = useRef<DropdownRef>(null);
 
+  /**
+   * 监听路径
+   * 有子路由则自动跳转第一个
+   * 分别记录父、子选项卡当前选择key
+   */
   useEffect(() => {
-    const pathRule = /^\/\w+$/.test(pathname) || /^\/\w+\/$/.test(pathname);
-    if (pathRule && children.length > 0) {
-      const type = pathname.match(/^\/[^/]*/);
-      navigate(`${type}/1`);
+    const pathArr = pathname.match(/\/\w+/g);
+    if (Array.isArray(pathArr)) {
+      const [parentTab, childrenTab] = pathArr;
+      if (!childrenTab && children.length > 0) {
+        navigate(`${parentTab}/1`);
+      }
+      setParentActiveKey(parentTab);
+      setChildrenActiveKey(pathname);
     }
-    setActionKey(pathname);
   }, [pathname]);
 
   /**
@@ -38,11 +49,11 @@ export function NavList(props: PropType) {
 
   /**
    * 获取当前父选项是否有子路由
-   * @param key 当前激活选项卡
+   * @param path 当前激活选项卡---路径决定
    */
-  function clickEvent(key: string) {
+  function clickEvent(path: string) {
     const childrenList = navList.filter(
-      (item) => item.path === key && item.children,
+      (item) => item.path === path && item.children,
     )[0]?.children;
     setChildren(childrenList || []);
   }
@@ -51,19 +62,11 @@ export function NavList(props: PropType) {
    * 选项卡视图遍历展示
    * @param list
    */
-  function view(list: NavItem[]) {
+  function tabView(list: NavItem[]) {
     const listView = list.map((item: NavItem) => {
       return (
         <Tabs.Tab
-          title={
-            <div
-              onClick={() => {
-                goRouter(item.path);
-              }}
-            >
-              {item.title}
-            </div>
-          }
+          title={<div onClick={() => goRouter(item.path)}>{item.title}</div>}
           key={item.path}
         />
       );
@@ -71,14 +74,46 @@ export function NavList(props: PropType) {
     return listView;
   }
 
+  /**
+   * 下拉路由导航
+   * @param list
+   */
+  function dropdownView(list: NavItem[]) {
+    return list.map((item) => (
+      <div
+        onClick={() => {
+          ref.current?.close();
+          clickEvent(item.path);
+          goRouter(item.path);
+        }}
+      >
+        {item.title}
+      </div>
+    ));
+  }
+
   return (
     <div className='NavList'>
       <div className='top'>
-        <Tabs onChange={(item) => clickEvent(item)}>{view(navList)}</Tabs>
-        <DownOutline className='icon' color='var(--adm-color-weak)' />
+        <Tabs activeKey={parentActiveKey} onChange={(item) => clickEvent(item)}>
+          {tabView(navList)}
+        </Tabs>
+        <Dropdown ref={ref}>
+          <Dropdown.Item arrow={<DownOutline />} key='DownOutline' title=''>
+            <div className='dropdownView'>
+              <div className='content'>{dropdownView(navList)}</div>
+              <UpOutline
+                onClick={() => {
+                  ref.current?.close();
+                }}
+                className='icon'
+              />
+            </div>
+          </Dropdown.Item>
+        </Dropdown>
       </div>
       {children.length > 0 && (
-        <Tabs activeKey={actionKey}>{view(children)}</Tabs>
+        <Tabs activeKey={childrenActiveKey}>{tabView(children)}</Tabs>
       )}
     </div>
   );
